7c91f829fbb8a15faee1457b7469db3ebe1c787e
luciorox11@outlook.com <luciodevinfo@gmail.com>
2020-12-28 03:44:40 -0300

    rename folder

---
+++ service/service.py
@@ -0,0 +1,29 @@
+#coding: utf-8
+# -*- coding:utf-8 -*-
+
+import time
+import pyautogui
+
+class service:
+    
+    def __init__(self):
+        self.error = None
+
+    def inputlist(self):
+        self.passcrack('./archives/wordlist.txt')
+
+    def passcrack(self, file_name):
+        word_list = open(file_name, 'r')
+        words = word_list.read()
+        word_list.close()
+        word_list = words.split("\n")
+        print('CLICK ON THE FIELD!')
+        time.sleep(5)
+
+        for word in word_list[0::]:
+            pyautogui.hotkey('ctrl', 'a')
+            time.sleep(1)
+            pyautogui.typewrite(word, interval=0.0001)
+            pyautogui.typewrite(['enter'])
+            print(word)
+
